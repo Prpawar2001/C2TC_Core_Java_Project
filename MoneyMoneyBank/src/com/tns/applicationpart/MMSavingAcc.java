f1c2d6735a319ed4be30ee8023edf41149506dfe
prpaw <prpaw@DESKTOP-5RK9R5K>
2022-03-09 16:08:34 +0530

    MoneyMoney Bank Application

---
+++ MoneyMoneyBank/src/com/tns/applicationpart/MMSavingAcc.java
@@ -0,0 +1,23 @@
+package com.tns.applicationpart;
+import com.tns.framework.*;
+
+public class MMSavingAcc extends SavingAcc {
+
+	final private float minbal=0;
+
+	public MMSavingAcc(int accNo, String accNm, float accBal, boolean isSalaried) {
+		super(accNo, accNm, accBal, isSalaried);
+		
+	}
+	
+	public void withdraw(float wd) {
+		super.withdraw(wd);
+	}
+
+@Override
+public String toString() {
+	return "MMSavingAcc [minbal=" + minbal + ", toString()=" + super.toString() + ", getClass()=" + getClass()
+			+ ", hashCode()=" + hashCode() + "]";
+}
+	
+}
